c3cf3a789a58e3e245c226cff72032e9223d1c45
Jessica <jessicamcav13@gmail.com>
2018-10-28 11:40:50 +0100

    c

---
+++ ejemploJSON.js
@@ -0,0 +1,13 @@
+'use strict'
+
+// JSON JavaScript Object Notation
+
+var pelicula = {
+    title:'Batman vs Superman',
+    year: 2017,
+    country: 'USA'
+};
+
+pelicula.title = "Superman Begins";
+
+console.log(pelicula.title);
